15e0f00c332cab81111f7b6b8f223f2e33f4ebb3
Nico Geesink <geesinknico@gmail.com>
2024-09-25 21:45:47 +0200

    Added a init function

--- cmd/git.go
+++ cmd/git.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
 )
 
 func CloneRepo(path string, url string) error {
@@ -11,7 +12,23 @@ func CloneRepo(path string, url string) error {
 		URL:      url,
 		Progress: os.Stdout,
 	})
+	if err != nil {
+		return err
+	}
 
+	return nil
+}
+
+func InitRepo(path string, url string) error {
+	repo, err := git.PlainInit(path, false)
+	if err != nil {
+		return err
+	}
+
+	_, err = repo.CreateRemote(&config.RemoteConfig{
+		Name: "origin",
+		URLs: []string{url},
+	})
 	if err != nil {
 		return err
 	}
