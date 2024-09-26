42ce152562fb9a443c156eb4267f9d1a17b960d3
Nico Geesink <geesinknico@gmail.com>
2024-09-26 15:41:09 +0200

    Added an init function

--- internal/flags/subcommands.go
+++ internal/flags/subcommands.go
@@ -16,6 +16,9 @@ func SetSubcommands() error {
 	newRepoPath := newRepoCmd.String("path", "./", "Path to the new repo")
 	newRepoUrl := newRepoCmd.String("url", "", "URL of the repo")
 
+	initCmd := flag.NewFlagSet("init", flag.ExitOnError)
+	initPath := initCmd.String("path", "./", "Path to the repo")
+
 	if len(os.Args) < 2 {
 		err := errors.New("Expected subcommand")
 		return err
@@ -44,6 +47,13 @@ func SetSubcommands() error {
 				return err
 			}
 		}
+		return nil
+
+	case "init":
+		if err := data.NewDataFile(*initPath); err != nil {
+			return err
+		}
+
 		return nil
 	}
 
