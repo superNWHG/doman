35b3d3e185c6d52b70f6d2ebb0a180689a5a4948
Nico Geesink <geesinknico@gmail.com>
2024-09-30 09:07:59 +0200

    Removed debug print statements

--- cmd/linkfiles.go
+++ cmd/linkfiles.go
@@ -1,13 +1,8 @@
 package cmd
 
-import (
-	"fmt"
-	"os"
-)
+import "os"
 
 func NewLink(oldPath []string, newPath []string) error {
-	fmt.Println("Old path:", oldPath)
-	fmt.Println("New path:", newPath)
 	for i := range oldPath {
 		if err := os.Symlink(oldPath[i], newPath[i]); err != nil {
 			return err
