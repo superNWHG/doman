807086d7ab14abdeb4783a59f970f71d9b4d3964
Nico Geesink <geesinknico@gmail.com>
2024-09-30 09:29:42 +0200

    Made a new link being copied first to the dotfiles

--- cmd/linkfiles.go
+++ cmd/linkfiles.go
@@ -4,7 +4,11 @@ import "os"
 
 func NewLink(oldPath []string, newPath []string) error {
 	for i := range oldPath {
-		if err := os.Symlink(oldPath[i], newPath[i]); err != nil {
+		if err := os.Rename(oldPath[i], newPath[i]); err != nil {
+			return err
+		}
+
+		if err := os.Symlink(newPath[i], oldPath[i]); err != nil {
 			return err
 		}
 	}
