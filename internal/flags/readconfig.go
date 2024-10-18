b286a2eb8de6fe9ab7cb926c81b9514e93e9145b
Nico Geesink <geesinknico@gmail.com>
2024-10-18 21:20:43 +0200

    Make loop more compact

--- internal/flags/readconfig.go
+++ internal/flags/readconfig.go
@@ -15,7 +15,7 @@ func readconfig(path string) error {
 
 	userConfig := reflect.Indirect(reflect.ValueOf(configValues))
 
-	for i := 0; i < userConfig.NumField(); i++ {
+	for i := range userConfig.NumField() {
 		fmt.Println(userConfig.Type().Field(i).Name)
 		for x := 0; x < userConfig.Type().Field(i).Type.NumField(); x++ {
 			fmt.Println(userConfig.Type().Field(i).Type.Field(x).Name, ":", userConfig.Field(i).Field(x))
