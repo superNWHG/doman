8116da5cf3ea5867a92bf174a778c2205f09384a
Nico Geesink <geesinknico@gmail.com>
2024-10-30 15:48:24 +0100

    Add a function to extract git credentials from local git

--- pkg/gitcredentials/gitcredentials.go
+++ pkg/gitcredentials/gitcredentials.go
@@ -1,7 +1,11 @@
 package gitcredentials
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 	"syscall"
 
 	"golang.org/x/term"
@@ -27,4 +31,63 @@ func GetGitCredentials() (error, string, string, string) {
 	}
 
 	return nil, name, mail, string(pass)
+}
+
+func GetGitCredentialHelper(url string) (string, string, string, error) {
+	credentialFile := filepath.Join(os.Getenv("HOME"), ".git-credentials")
+
+	credentialContent, err := os.ReadFile(credentialFile)
+	if err != nil {
+		return "", "", "", err
+	}
+	if !strings.Contains(string(credentialContent), url) {
+		err := errors.New(url + " not in credential file")
+		return "", "", "", err
+	}
+
+	credentials := strings.Split(string(credentialContent), "\n")
+
+	var urlCredential string
+	for i := range credentials {
+		if strings.Contains(credentials[i], url) {
+			urlCredential = string(credentials[i])
+			break
+		}
+	}
+
+	noUrl := strings.Replace(urlCredential, "https://", "", -1)
+	noUrl = strings.Replace(noUrl, "@"+url, "", -1)
+
+	var name, password string
+	for i, v := range noUrl {
+		if string(v) == ":" {
+			name = noUrl[:i]
+			password = noUrl[i+1:]
+		}
+	}
+
+	gitconfigFile := filepath.Join(os.Getenv("HOME"), ".gitconfig")
+
+	gitconfigContent, err := os.ReadFile(gitconfigFile)
+	if err != nil {
+		return "", "", "", err
+	}
+	if !strings.Contains(string(gitconfigContent), "email = ") {
+		err := errors.New("email not in gitconfig file")
+		return "", "", "", err
+	}
+
+	var mail string
+	gitConfigLines := strings.Split(string(gitconfigContent), "\n")
+	for i := range gitConfigLines {
+		if strings.Contains(gitConfigLines[i], "email = ") {
+			for j, v := range gitConfigLines[i] {
+				if string(v) == "=" {
+					mail = gitConfigLines[i][j+2:]
+				}
+			}
+		}
+	}
+
+	return name, mail, password, nil
 }
