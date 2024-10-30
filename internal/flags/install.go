2e1ca1f027e219e308c8a8840eb92503efa4bca3
Nico Geesink <geesinknico@gmail.com>
2024-10-30 13:48:26 +0100

    Make filepath and wait for enter correct

--- internal/flags/install.go
+++ internal/flags/install.go
@@ -2,12 +2,14 @@ package flags
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/superNWHG/doman/internal/data"
 	"github.com/superNWHG/doman/internal/packages"
 )
 
 func install(path string, installNames []string, os string) error {
+	path = filepath.Join(path, "dotfiles.json")
 	names, _, _, err := data.ReadDataFile(path)
 	if err != nil {
 		return err
@@ -18,8 +20,9 @@ func install(path string, installNames []string, os string) error {
 	}
 
 	for i := range installNames {
-		fmt.Print("Please select a package to install for", installNames[i], "\nPress [ENTER] to continue")
-		fmt.Scan()
+		fmt.Print("Please select a package to install for ", installNames[i], "\nPress [ENTER] to continue")
+		var input string
+		fmt.Scanln(&input)
 		pkg, err := packages.Search(os, installNames[i])
 		if err != nil {
 			return err
