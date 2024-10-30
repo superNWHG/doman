7b29a5477d667424caae7cb9963aa8f19ed65a5b
Nico Geesink <geesinknico@gmail.com>
2024-10-30 12:31:55 +0100

    Add a packages package to interact with package manager

---
+++ internal/packages/packages.go
@@ -0,0 +1,65 @@
+package packages
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/ktr0731/go-fuzzyfinder"
+)
+
+type packageManager struct {
+	Command     string
+	searchFlag  string
+	installFlag string
+}
+
+func search(p *packageManager, query string) ([]string, error) {
+	cmd := exec.Command(p.Command, p.searchFlag, query)
+
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(output), "\n")
+	var packages []string
+	packages = append(packages, lines...)
+	return packages, nil
+}
+
+func install(p *packageManager, program string) error {
+	cmd := exec.Command(p.Command, p.installFlag, program)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Search(os string, query string) error {
+	switch os {
+	case "arch":
+		packages, err := search(&packageManager{Command: "pacman", searchFlag: "-Ssq"}, query)
+		if err != nil {
+			return err
+		}
+
+		installPkg, err := fuzzyfinder.Find(packages, func(i int) string {
+			return packages[i]
+		})
+		if err != nil {
+			return err
+		}
+
+		if err := install(&packageManager{Command: "pacman", installFlag: "-S"}, packages[installPkg]); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
