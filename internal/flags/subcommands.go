acc149044d253dc549eaa772998b00e7608d6d85
Nico Geesink <geesinknico@gmail.com>
2024-10-31 22:51:23 +0100

    Add global path flag without pflag to make it usable for flag defaults

--- internal/flags/subcommands.go
+++ internal/flags/subcommands.go
@@ -3,6 +3,7 @@ package flags
 import (
 	"errors"
 	"os"
+	"strings"
 
 	"github.com/spf13/pflag"
 	"github.com/superNWHG/doman/internal/config"
@@ -12,11 +13,8 @@ import (
 type (
 	Defaults struct {
 		NewRepo `toml:"NewRepo"`
-		Init    `toml:"Init"`
 		Add     `toml:"Add"`
-		Read    `toml:"Read"`
 		Sync    `toml:"Sync"`
-		Link    `toml:"Link"`
 		Edit    `toml:"Edit"`
 		Config  `toml:"Config"`
 		Install `toml:"Install"`
@@ -29,20 +27,12 @@ type (
 		NewRepoPath     string `default:"./" toml:"path"`
 	}
 
-	Init struct {
-		InitPath string `default:"./" toml:"path"`
-	}
-
 	Add struct {
 		AddName     string `default:"" toml:"addName"`
 		AddEntry    string `default:"" toml:"addEntry"`
 		AddPath     string `default:"./" toml:"addPath"`
 		AddExisting bool   `default:"false" toml:"addExisting"`
 		AddFormat   bool   `default:"true" toml:"addFormat"`
-	}
-
-	Read struct {
-		ReadPath string `default:"./" toml:"readPath"`
 	}
 
 	Sync struct {
@@ -54,10 +44,6 @@ type (
 		SyncPush    bool     `default:"false" toml:"syncPush"`
 	}
 
-	Link struct {
-		LinkPath string `default:"./" toml:"linkPath"`
-	}
-
 	Edit struct {
 		EditName   string `default:"" toml:"editName"`
 		EditEditor string `default:"" toml:"editEditor"`
@@ -66,9 +52,8 @@ type (
 	}
 
 	Config struct {
-		configPath string `default:"./" toml:"configPath"`
-		ConfigNew  bool   `default:"false" toml:"configNew"`
-		ConfigRead bool   `default:"false" toml:"configRead"`
+		ConfigNew  bool `default:"false" toml:"configNew"`
+		ConfigRead bool `default:"false" toml:"configRead"`
 	}
 
 	Install struct {
@@ -91,55 +76,62 @@ func getDefaults(path string) (*Defaults, error) {
 }
 
 func SetSubcommands() error {
-	defaults, err := getDefaults("./")
+	var path string
+	for i, v := range os.Args {
+		if strings.Contains(v, "path") {
+			if strings.Contains(v, "=") {
+				path = strings.Split(v, "=")[1]
+			} else {
+				i++
+				path = os.Args[i]
+			}
+
+			os.Args = append(os.Args[:i], os.Args[i+1:]...)
+			break
+		}
+		path = "./"
+	}
+
+	defaults, err := getDefaults(path)
 	if err != nil {
 		return err
 	}
 
 	newRepoCmd := pflag.NewFlagSet("new", pflag.ExitOnError)
 	newRepoUrl := newRepoCmd.String("url", defaults.NewRepoUrl, "URL to the repository")
-	newRepoPath := newRepoCmd.String("path", defaults.NewRepoPath, "Path to the dotfiles directory")
 	newRepoClone := newRepoCmd.Bool("clone", defaults.NewRepoClone, "Clone a repository instead of initializing a new one")
 	newRepoDataFile := newRepoCmd.Bool("datafile", defaults.NewRepoDataFile, "Don't create a data file to keep track of your dotfiles")
 
 	initCmd := pflag.NewFlagSet("init", pflag.ExitOnError)
-	initPath := initCmd.String("path", defaults.InitPath, "Path to the dotfiles directory")
 
 	addCmd := pflag.NewFlagSet("add", pflag.ExitOnError)
 	addName := addCmd.String("name", defaults.AddName, "Name of the dotfile entry")
 	addEntry := addCmd.String("entry", defaults.AddEntry, "Path to the new dotfile entry")
-	addPath := addCmd.String("path", defaults.AddPath, "Path to the dotfiles directory")
 	addExisting := addCmd.Bool("existing", defaults.AddExisting, "Add an existing file in your dotfiles directory")
 	addFormat := addCmd.Bool("format", defaults.AddFormat, "Automatically format dotfiles.json")
 
 	readCmd := pflag.NewFlagSet("read", pflag.ExitOnError)
-	readPath := readCmd.String("path", defaults.ReadPath, "Path to the dotfiles directory")
 
 	syncCmd := pflag.NewFlagSet("sync", pflag.ExitOnError)
 	syncMessage := syncCmd.String("message", defaults.SyncMessage, "Custom commit message")
-	syncPath := syncCmd.String("path", defaults.SyncPath, "Path to the dotfiles directory")
 	syncFiles := syncCmd.StringSlice("files", defaults.SyncFiles, "Files you want to sync (leave empty to sync all)")
 	syncAuth := syncCmd.Bool("authentication", defaults.SyncAuth, "Ask for username and password")
 	syncGitAuth := syncCmd.Bool("gitauthentication", defaults.SyncGitAuth, "Use local git credentials")
 	syncPush := syncCmd.Bool("push", defaults.SyncPush, "Automatically push to the remote repository")
 
 	linkCmd := pflag.NewFlagSet("link", pflag.ExitOnError)
-	linkPath := linkCmd.String("path", defaults.LinkPath, "Path to the dotfiles directory")
 
 	editCmd := pflag.NewFlagSet("edit", pflag.ExitOnError)
 	editName := editCmd.String("name", defaults.EditName, "Name of the dotfile entry to edit")
 	editEditor := editCmd.String("editor", defaults.EditEditor, "Editor you want to use (leave empty to use $EDITOR)")
-	editPath := editCmd.String("path", defaults.EditPath, "Path to the dotfiles directory")
 	editFormat := editCmd.Bool("format", defaults.EditFormat, "Automatically format dotfiles.json")
 
 	configCmd := pflag.NewFlagSet("config", pflag.ExitOnError)
-	configPath := configCmd.String("path", defaults.configPath, "Path to the dotfiles directory")
 	configNew := configCmd.Bool("new", defaults.ConfigNew, "Create a new configuration file")
 	configRead := configCmd.Bool("read", defaults.ConfigRead, "Read the configuration file")
 
 	installCmd := pflag.NewFlagSet("install", pflag.ExitOnError)
 	installOs := installCmd.String("os", defaults.InstallOs, "Specify the os")
-	installPath := installCmd.String("path", defaults.InstallPath, "Path to the dotfiles directory")
 	installNames := installCmd.StringSlice("names", defaults.InstallNames, "Specify the names of the packages you want to install")
 
 	if len(os.Args) < 2 {
@@ -160,24 +152,24 @@ func SetSubcommands() error {
 		}
 
 		if *newRepoClone {
-			if err := newCloneRepo(*newRepoPath, *newRepoUrl); err != nil {
+			if err := newCloneRepo(path, *newRepoUrl); err != nil {
 				return err
 			}
 		} else {
-			if err := newInitRepo(*newRepoPath, *newRepoUrl); err != nil {
+			if err := newInitRepo(path, *newRepoUrl); err != nil {
 				return err
 			}
 		}
 
 		if *newRepoDataFile {
-			if err := data.NewDataFile(*newRepoPath); err != nil {
+			if err := data.NewDataFile(path); err != nil {
 				return err
 			}
 		}
 		return nil
 
 	case "init":
-		if err := data.NewDataFile(*initPath); err != nil {
+		if err := data.NewDataFile(path); err != nil {
 			return err
 		}
 
@@ -194,11 +186,11 @@ func SetSubcommands() error {
 		}
 
 		if *addExisting {
-			if err := addExistingData(*addPath, *addName, *addEntry, *addFormat); err != nil {
+			if err := addExistingData(path, *addName, *addEntry, *addFormat); err != nil {
 				return err
 			}
 		} else {
-			if err := addData(*addPath, *addName, *addEntry, *addFormat); err != nil {
+			if err := addData(path, *addName, *addEntry, *addFormat); err != nil {
 				return err
 			}
 		}
@@ -210,7 +202,7 @@ func SetSubcommands() error {
 			return err
 		}
 
-		if err := readData(*readPath); err != nil {
+		if err := readData(path); err != nil {
 			return err
 		}
 
@@ -226,7 +218,7 @@ func SetSubcommands() error {
 			return err
 		}
 
-		if err := data.Sync(*syncPath, *syncMessage, *syncPush, *syncAuth, *syncGitAuth, *syncFiles); err != nil {
+		if err := data.Sync(path, *syncMessage, *syncPush, *syncAuth, *syncGitAuth, *syncFiles); err != nil {
 			return err
 		}
 
@@ -237,7 +229,7 @@ func SetSubcommands() error {
 			return err
 		}
 
-		if err := data.LinkData(*linkPath); err != nil {
+		if err := data.LinkData(path); err != nil {
 			return err
 		}
 
@@ -253,7 +245,7 @@ func SetSubcommands() error {
 			return err
 		}
 
-		if err := data.EditData(*editPath, *editName, *editEditor, *editFormat); err != nil {
+		if err := data.EditData(path, *editName, *editEditor, *editFormat); err != nil {
 			return err
 		}
 
@@ -275,13 +267,13 @@ func SetSubcommands() error {
 		}
 
 		if *configNew {
-			if err := config.NewConfig(*configPath, Defaults{}); err != nil {
+			if err := config.NewConfig(path, Defaults{}); err != nil {
 				return err
 			}
 		}
 
 		if *configRead {
-			if err := readconfig(*configPath, &Defaults{}); err != nil {
+			if err := readconfig(path, &Defaults{}); err != nil {
 				return err
 			}
 		}
@@ -293,7 +285,7 @@ func SetSubcommands() error {
 			return err
 		}
 
-		if err := install(*installPath, *installNames, *installOs); err != nil {
+		if err := install(path, *installNames, *installOs); err != nil {
 			return err
 		}
 
