323e25ca4481b28961491c143dccc7591f738af8
Nico Geesink <geesinknico@gmail.com>
2024-10-31 12:15:51 +0100

    Add goroutines to read 2 files at the same time

--- pkg/gitcredentials/gitcredentials.go
+++ pkg/gitcredentials/gitcredentials.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"syscall"
 
 	"golang.org/x/term"
@@ -35,58 +36,77 @@ func AskGitCredentials() (string, string, string, error) {
 
 func GetGitCredentials(url string) (string, string, string, error) {
 	credentialFile := filepath.Join(os.Getenv("HOME"), ".git-credentials")
-
-	credentialContent, err := os.ReadFile(credentialFile)
-	if err != nil {
-		return "", "", "", err
-	}
-	if !strings.Contains(string(credentialContent), url) {
-		err := errors.New(url + " not in credential file")
-		return "", "", "", err
-	}
-
-	credentials := strings.Split(string(credentialContent), "\n")
-
-	var urlCredential string
-	for i := range credentials {
-		if strings.Contains(credentials[i], url) {
-			urlCredential = string(credentials[i])
-			break
-		}
-	}
-
-	noUrl := strings.Replace(urlCredential, "https://", "", -1)
-	noUrl = strings.Replace(noUrl, "@"+url, "", -1)
-
-	var name, password string
-	for i, v := range noUrl {
-		if string(v) == ":" {
-			name = noUrl[:i]
-			password = noUrl[i+1:]
-		}
-	}
-
 	gitconfigFile := filepath.Join(os.Getenv("HOME"), ".gitconfig")
 
-	gitconfigContent, err := os.ReadFile(gitconfigFile)
-	if err != nil {
-		return "", "", "", err
-	}
-	if !strings.Contains(string(gitconfigContent), "email = ") {
-		err := errors.New("email not in gitconfig file")
-		return "", "", "", err
-	}
+	var name, password, mail string
+	var error error
 
-	var mail string
-	gitConfigLines := strings.Split(string(gitconfigContent), "\n")
-	for i := range gitConfigLines {
-		if strings.Contains(gitConfigLines[i], "email = ") {
-			for j, v := range gitConfigLines[i] {
-				if string(v) == "=" {
-					mail = gitConfigLines[i][j+2:]
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		credentialContent, err := os.ReadFile(credentialFile)
+		if err != nil {
+			error = err
+			return
+		}
+		if !strings.Contains(string(credentialContent), url) {
+			err := errors.New(url + " not in credential file")
+			error = err
+			return
+		}
+
+		credentials := strings.Split(string(credentialContent), "\n")
+
+		var urlCredential string
+		for i := range credentials {
+			if strings.Contains(credentials[i], url) {
+				urlCredential = string(credentials[i])
+				break
+			}
+		}
+
+		noUrl := strings.Replace(urlCredential, "https://", "", -1)
+		noUrl = strings.Replace(noUrl, "@"+url, "", -1)
+
+		for i, v := range noUrl {
+			if string(v) == ":" {
+				name = noUrl[:i]
+				password = noUrl[i+1:]
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		gitconfigContent, err := os.ReadFile(gitconfigFile)
+		if err != nil {
+			error = err
+			return
+		}
+		if !strings.Contains(string(gitconfigContent), "email = ") {
+			err := errors.New("email not in gitconfig file")
+			error = err
+			return
+		}
+
+		gitConfigLines := strings.Split(string(gitconfigContent), "\n")
+		for i := range gitConfigLines {
+			if strings.Contains(gitConfigLines[i], "email = ") {
+				for j, v := range gitConfigLines[i] {
+					if string(v) == "=" {
+						mail = gitConfigLines[i][j+2:]
+					}
 				}
 			}
 		}
+	}()
+
+	wg.Wait()
+	if error != nil {
+		return "", "", "", error
 	}
 
 	return name, mail, password, nil
